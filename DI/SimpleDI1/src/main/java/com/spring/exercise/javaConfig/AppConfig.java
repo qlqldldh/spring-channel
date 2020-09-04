d51bf5916ae7a040e2949d307fe5d8e8715cf445
Jinsu Park <qlqldldh@naver.com>
2020-09-04 22:11:44 +0900

    Update AppConfig.java

--- DI/SimpleDI1/src/main/java/com/spring/exercise/javaConfig/AppConfig.java
+++ DI/SimpleDI1/src/main/java/com/spring/exercise/javaConfig/AppConfig.java
@@ -2,7 +2,7 @@ package com.spring.exercise.javaConfig;
 
 import org.springframework.context.annotation.Bean;
 
-//@Configuration // @Configuration이 제외되어야 오히려 에러 없음. 아이러니
+//@Configuration // @Configuration이 제외되어야 오히려 에러 없음. 아이러니 // 해결 : cglib 의존성을 추가해야 함
 public class AppConfig {
 	@Bean
 	UserCheck userCheck() {
